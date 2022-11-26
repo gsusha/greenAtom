97488a45caaad5119d5c9639a98053a58214a14e
RUzai <RUzairov@gmail.com>
2022-11-26 10:30:05 +0300

    ts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
-import App from './app/App.jsx';
+import App from './app/App.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
