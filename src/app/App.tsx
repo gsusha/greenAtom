8b6e4a84b1e5cb050ef0924ac2534322ca089002
gsusha <x.dyachenko@gmail.com>
2022-11-26 16:31:09 +0300

    main fix

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
-import UserMain from './pages/user/';
 import AdminMain from './pages/admin/';
 import UserLayout from './layout/user/UserLayout';
 import './assets/fonts/fonts.css';
 import imgStart from './assets/images/user_start.png';
+import UserMain from './pages/user/main/UserMain';
 
 // import imgEnd from './assets/images/user_end.jpg';
 
