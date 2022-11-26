2387cb09919029e1eb6471ae424e52e04d157309
gsusha <x.dyachenko@gmail.com>
2022-11-27 00:15:34 +0300

    part of redux v2

---
+++ src/app/components/loader/loader.tsx
@@ -0,0 +1,12 @@
+import { Circles } from 'react-loader-spinner';
+import * as React from 'react';
+
+function Loader() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw' }}>
+      <Circles />;
+    </div>
+  );
+}
+
+export default Loader;
