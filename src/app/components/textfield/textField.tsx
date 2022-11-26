1e11d017c223a5b140e6879d78113e642e9ca070
RUzai <RUzairov@gmail.com>
2022-11-27 00:09:59 +0300

    input

---
+++ src/app/components/textfield/textField.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.scss';
+
+interface Props {
+  type?: string;
+  children?: string;
+}
+
+export default function TextField(props: Props) {
+  return <input type={props.type} placeholder={props.children}></input>;
+}
--- src/app/pages/user/first_step/FirstStep.tsx
+++ src/app/pages/user/first_step/FirstStep.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import Title from '../../../components/title/title';
+import TextField from '../../../components/textfield/textField';
 import Progress from '../../../components/progress/progress';
 import Button from '../../../components/button/button';
 import { CgArrowLongRight } from 'react-icons/cg';
 
 function FirstStep() {
   return (
-    <div>
+    <form>
       <Title style={{ marginBottom: 10 }}>Шаг 1/3</Title>
       <Progress step={1} />
       <div className="description" style={{ marginBottom: 22 }}>
         Заполни <span>личные данные</span>:
       </div>
+      <TextField type={'text'}>Имя *</TextField>
+      <TextField type={'tel'}>Телефон *</TextField>
+      <TextField type={'text'}>Telegram</TextField>
       <div className="description" style={{ marginBottom: 22 }}>
         И выбери <span>программу подготовки</span>, которая тебе интересна:
       </div>
       <Button name="Дальше" to="/second" icon={<CgArrowLongRight />} />
-    </div>
+    </form>
   );
 }
 
