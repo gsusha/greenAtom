8f59a1724e2420bb833b088d162bdef118f06069
RUzai <RUzairov@gmail.com>
2022-11-27 11:38:05 +0300

    share buttons

--- src/app/pages/user/end/UserEnd.tsx
+++ src/app/pages/user/end/UserEnd.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
 import Title from '../../../components/title/title';
-import Button from '../../../components/button/button';
 import { FaShareAlt } from 'react-icons/fa';
 import logo from '../../../assets/images/rosatom_logo.svg';
 import imgEnd from '../../../assets/images/user_end.jpg';
 import UserLayout from '../../../layout/user/UserLayout';
+import '../main/styles.scss';
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  TelegramIcon,
+  TelegramShareButton,
+  VKIcon,
+  VKShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from 'react-share';
+import Constants from '../../../utils/constants';
 
 function UserEnd() {
+  // TODO: generate
+  const shareUrl = Constants.API_URL + '/form?id=0&inviterId=0';
+
   return (
     <UserLayout img={imgEnd}>
       <div className="user-main-wrapper" style={{ justifyContent: 'space-between' }}>
@@ -19,10 +35,29 @@ function UserEnd() {
             Скорее поделись формой с друзьями, чтобы и они получили возможность попасть на{' '}
             <span>оплачиваемую стажировку</span> в нашей компании!
           </div>
-
-          <Button to="/first" icon={<FaShareAlt />}>
-            Поделиться
-          </Button>
+          <div className={'share'}>
+            <span className={'share__title'}>Поделиться:</span>
+            <div className={'share__buttons'}>
+              <FacebookShareButton url={shareUrl}>
+                <FacebookIcon size={48} round />
+              </FacebookShareButton>
+              <WhatsappShareButton url={shareUrl}>
+                <WhatsappIcon size={48} round />
+              </WhatsappShareButton>
+              <VKShareButton url={shareUrl}>
+                <VKIcon size={48} round />
+              </VKShareButton>
+              <TelegramShareButton url={shareUrl}>
+                <TelegramIcon size={48} round />
+              </TelegramShareButton>
+              <EmailShareButton url={shareUrl}>
+                <EmailIcon size={48} round />
+              </EmailShareButton>
+            </div>
+            <p className={'share__desc'}>
+              Чем больше друзей заполнят форму по вашей ссылке, тем выше вероятность получить приглашение!
+            </p>
+          </div>
         </div>
         <div>
           <img src={logo} alt="Rosatom logo" style={{ margin: 0 }} />
